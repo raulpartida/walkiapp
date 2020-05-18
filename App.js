85ea0151d97de0ffa7f5165298bbc79ecfa5aaf6
Raul Partida <raulmass2@gmail.com>
2020-05-17 23:16:02 -0500

    Corrección de acceso en login

--- App.js
+++ App.js
@@ -59,7 +59,7 @@ const TabStackScreen = () => (
   </Tabs.Navigator>
 );
 
-async function _loginHandle(user = '', password = '') {
+async function _loginHandle(user = '', password = '', dispatch) {
   try {
     let hash = await fetch(baseURL + '/user/login', {
       method: 'POST',
@@ -73,13 +73,21 @@ async function _loginHandle(user = '', password = '') {
       }),
     })
       .then(response => {
-        return response.text();
+        var contentType = response.headers.get('content-type');
+        if (contentType !== 'application/json; charset=utf-8')
+          return response.text();
+        else return null;
       })
       .catch(error => {
         console.error('Error:', error);
       });
     try {
-      await AsyncStorage.setItem('token', hash);
+      if (hash != null) {
+        await AsyncStorage.setItem('token', hash);
+        dispatch({type: 'SIGN_IN', token: hash});
+      } else {
+        console.log('Error de acceso');
+      }
     } catch (error) {
       console.log(error.message);
     }
@@ -92,7 +100,6 @@ const App: () => React$Node = () => {
 
   const [state, dispatch] = React.useReducer(
     (prevState, action) => {
-      console.log(action);
       switch (action.type) {
         case 'RESTORE_TOKEN':
           return {
@@ -128,14 +135,14 @@ const App: () => React$Node = () => {
       signIn: (user, password) => {
         let userToken = null;
         if (user !== '' && password !== '')
-          userToken = _loginHandle(user, password);
-
-        dispatch({type: 'SIGN_IN', token: userToken});
+          userToken = _loginHandle(user, password, dispatch);
       },
       signOut: async () => {
         try {
           dispatch({type: 'SIGN_OUT'});
-          await AsyncStorage.removeItem('token');
+          await AsyncStorage.getAllKeys().then(keys =>
+            AsyncStorage.multiRemove(keys),
+          );
         } catch (error) {}
       },
     }),
